e81a5b2ae422b5fa37cc726bb4b7286b1566a579
André Betetto <andre.betetto@unesp.br>
2023-02-19 12:28:59 -0300

    newFiles

---
+++ RecursosHumanos.dpr
@@ -0,0 +1,14 @@
+program RecursosHumanos;
+
+uses
+  Vcl.Forms,
+  TelaInicial in 'TelaInicial.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
