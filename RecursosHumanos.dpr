a697c96995df4b9aa449f0643df00862265b64e3
André Betetto <andre.betetto@unesp.br>
2023-02-19 13:50:09 -0300

    update

--- RecursosHumanos.dpr
+++ RecursosHumanos.dpr
@@ -3,7 +3,8 @@ program RecursosHumanos;
 uses
   Vcl.Forms,
   TelaInicial in 'TelaInicial.pas' {Form1},
-  insert in 'insert.pas' {Form2};
+  insert in 'insert.pas' {Form2},
+  Tabelas in 'Tabelas.pas' {Form3};
 
 {$R *.res}
 
@@ -12,5 +13,6 @@ begin
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm1, Form1);
   Application.CreateForm(TForm2, Form2);
+  Application.CreateForm(TForm3, Form3);
   Application.Run;
 end.
